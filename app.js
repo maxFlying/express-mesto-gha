981ed858b7b7f5ebade6ddb6ac114089f1259ad6
Maxim G <golubms2010@yandex.ru>
2022-09-11 21:07:08 +0300

    ПР14: истравить ошибки автотеста

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(cookieParser());
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
@@ -28,13 +35,6 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
   }),
 }), addUser);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
 
 app.use(auth);
 
