795b625c48ddbe5a512c2b9913e68d9a8dfaf8d0
Maxim G <golubms2010@yandex.ru>
2022-09-12 22:12:48 +0300

    ПР14: истравить ошибки автотеста(6)

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const validator = require('validator');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
 const userSchema = new mongoose.Schema({
@@ -9,7 +8,6 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
-    validate: validator.isEmail,
   },
   about: {
     type: String,
