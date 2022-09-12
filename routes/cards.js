6aa44316ad83f61c18b0529faf7b5780fbbdd214
Maxim G <golubms2010@yandex.ru>
2022-09-12 21:36:36 +0300

    ПР14: истравить ошибки автотеста(5)

--- routes/cards.js
+++ routes/cards.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const {
   getCard,
@@ -8,9 +9,27 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCard);
-router.post('/cards', addCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dilikeCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
+    about: Joi.string().required().pattern(/(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/),
+  }),
+}), addCard);
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().min(24).max(24),
+  }),
+}), deleteCard);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().min(24).max(24),
+  }),
+}), likeCard);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().min(24).max(24),
+  }),
+}), dilikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const {
   getUser,
@@ -9,8 +10,22 @@ const {
 
 router.get('/users', getUser);
 router.get('/users/me', getMyInfo);
-router.get('/users/:userId', getUserById);
-router.patch('/users/me', editProfile);
-router.patch('/users/me/avatar', editAvatar);
+router.get('/users/:userId', celebrate({
+  body: Joi.object().keys({
+    userId: Joi.string().required().min(24).max(24),
+  }),
+}), getUserById);
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().min(2).max(30),
+  }),
+}), editProfile);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().required().pattern(/(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/),
+  }),
+}), editAvatar);
 
 module.exports = router;
